b50ed34b21a7ece9774e22e5fbc1a16bd71448fe
vabigor <vab.igor@mail.ru>
2023-10-26 13:02:24 +0300

    Initial commit

---
+++ src/test/java/com/example/constructor/ConstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.constructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
